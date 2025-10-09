40d72c34e8ba56bb3b2164878bf5c806c219529e
skewmann-ux <sanusiolayinka7@gmail.com>
2025-10-09 23:37:11 +0100

    Looking into styled-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import GlobalStyles from "../styles/GlobalStyles";
 
 const H1 = styled.h1`
   font-size: 30px;
@@ -10,17 +11,20 @@ const Button = styled.button`
   padding: 1.2rem 1.6rem;
   font-weight: 500;
   border: none;
-  border-radius: 7px;
-  background-color: purple;
-  color: white;
+  border-radius: var(--border-radius-sm);
+  background-color: var(--color-brand-600);
+  color: var(--color-brand-50);
   margin: 20px;
+  box-shadow: var(--shadow-sm);
   cursor: pointer;
 `;
 
 const Input = styled.input`
-  border: 1px solid #ddd;
-  border-radius: 5px;
+  border: 1px solid var(--color-grey-300);
+  background-color: var(--color-grey-0);
+  border-radius: var(--border-radius-sm);
   padding: 0.8rem 1.2rem;
+  box-shadow: var(--shadow-sm);
 `;
 
 const StyledApp = styled.div`
@@ -30,12 +34,15 @@ const StyledApp = styled.div`
 
 function App() {
   return (
-    <StyledApp>
-      <H1>The Wild Oasis</H1>
-      <Button>Check In</Button>
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+        <Button>Check In</Button>
 
-      <Input type="number" />
-    </StyledApp>
+        <Input type="number" />
+      </StyledApp>
+    </>
   );
 }
 
