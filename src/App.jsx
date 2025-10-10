ae14a7ec8e8948c6cfaf3f005434cdc7b20d3c67
skewmann-ux <sanusiolayinka7@gmail.com>
2025-10-10 16:43:54 +0100

    //

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,10 @@
 import styled from "styled-components";
 import GlobalStyles from "../styles/GlobalStyles";
-
+import Button from "../ui/Button";
+import Input from "../ui/Input";
 const H1 = styled.h1`
   font-size: 30px;
   font-weight: 600;
-`;
-
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius: var(--border-radius-sm);
-  background-color: var(--color-brand-600);
-  color: var(--color-brand-50);
-  margin: 20px;
-  box-shadow: var(--shadow-sm);
-  cursor: pointer;
-`;
-
-const Input = styled.input`
-  border: 1px solid var(--color-grey-300);
-  background-color: var(--color-grey-0);
-  border-radius: var(--border-radius-sm);
-  padding: 0.8rem 1.2rem;
-  box-shadow: var(--shadow-sm);
 `;
 
 const StyledApp = styled.div`
