9d4628cf8b969a8a5f7a8516e74c33547688282f
Sanusi Olayinka <222006189+olayinkasanusi@users.noreply.github.com>
2025-10-30 23:12:04 +0100

    implemented the modal window

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -1,5 +1,22 @@
+import { useState } from "react";
+import Button from "../../ui/Button";
+import CreateCabinForm from "./CreateCabinForm";
+import Modal from "../../ui/Modal";
+
 function AddCabin() {
-  return;
+  const [isOpenModal, setIsOpenModal] = useState(false);
+  return (
+    <div>
+      <Button onClick={() => setIsOpenModal((show) => !show)}>
+        Add new cabin
+      </Button>
+      {isOpenModal && (
+        <Modal>
+          <CreateCabinForm />
+        </Modal>
+      )}
+    </div>
+  );
 }
 
 export default AddCabin;
--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/prop-types */
+import { HiXMark } from "react-icons/hi2";
 import styled from "styled-components";
 
 const StyledModal = styled.div`
@@ -49,8 +51,17 @@ const Button = styled.button`
   }
 `;
 
-function Modal() {
-  return <StyledModal>Modal</StyledModal>;
+function Modal({ children }) {
+  return (
+    <Overlay>
+      <StyledModal>
+        <Button>
+          <HiXMark />
+        </Button>
+        <div>{children}</div>
+      </StyledModal>
+    </Overlay>
+  );
 }
 
 export default Modal;
