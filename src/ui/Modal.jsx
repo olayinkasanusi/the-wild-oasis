9da82fdd0737d90ac0599d4ad2afad38f7ad2e0b
Sanusi Olayinka <222006189+olayinkasanusi@users.noreply.github.com>
2025-10-31 22:09:52 +0100

    //

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,12 +1,5 @@
 /* eslint-disable react/prop-types */
-import {
-  cloneElement,
-  createContext,
-  useContext,
-  useEffect,
-  useRef,
-  useState,
-} from "react";
+import { cloneElement, createContext, useContext, useState } from "react";
 import { createPortal } from "react-dom";
 import { HiXMark } from "react-icons/hi2";
 import styled from "styled-components";
