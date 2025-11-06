b152e419d7f39e28fe7c906e528feca7e4c2aabb
Sanusi Olayinka <222006189+olayinkasanusi@users.noreply.github.com>
2025-11-06 08:53:22 +0100

    pagination feature added

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,27 +1,37 @@
+import { PAGE_SIZE } from "../utils/constants";
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
-export async function getBookings(filter, sortBy) {
+export async function getBookings(filter, sortBy, page) {
   let query = supabase
     .from("bookings")
-    .select("*, cabins(name), guests(fullName, email)");
+    .select("*, cabins(name), guests(fullName, email)", { count: "exact" });
 
   //FILTER
   if (filter) query = query[filter.method || "eq"](filter.field, filter.value);
 
+  //Sort
   if (sortBy)
     query = query.order(sortBy.field, {
       ascending: sortBy.direction === "asc",
     });
 
-  const { data, error } = await query;
+  //Pagination
+  if (page) {
+    const from = PAGE_SIZE * (page - 1);
+    const to = from + PAGE_SIZE - 1;
+
+    query = query.range(from, to);
+  }
+
+  const { data, error, count } = await query;
 
   if (error) {
     console.error(error);
     throw new Error("Bookings couldn't be loaded");
   }
 
-  return data;
+  return { data, count };
 }
 
 export async function getBooking(id) {
