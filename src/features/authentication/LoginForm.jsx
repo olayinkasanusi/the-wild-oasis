ff06ee3913c854705466970520814322e44f90f4
Sanusi Olayinka <222006189+olayinkasanusi@users.noreply.github.com>
2025-11-09 13:14:13 +0100

    //

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("sanusiolayinka7@gmail.com");
-  const [password, setPassword] = useState("Olayinka123");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const { login, loggingIn } = useLogin();
 
