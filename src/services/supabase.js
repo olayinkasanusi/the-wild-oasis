8592cdacb0a9a5c3a6370c338f996494620880c5
Sanusi Olayinka <222006189+olayinkasanusi@users.noreply.github.com>
2025-11-09 22:38:15 +0100

    //

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
-export const supabaseUrl = import.meta.env.VITE_APP_SUPABASE_KEY;
-const supabaseKey = import.meta.env.VITE_APP_SUPABASE_KEY;
+// export const supabaseUrl = import.meta.env.VITE_APP_SUPABASE_KEY;
+// const supabaseKey = import.meta.env.VITE_APP_SUPABASE_KEY;
+
+export const supabaseUrl = "https://uwrghcifmvhsttlfkhgv.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV3cmdoY2lmbXZoc3R0bGZraGd2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAyMjQ0NDksImV4cCI6MjA3NTgwMDQ0OX0.pbEa7P9WdzOBNwhs8RSnf80l95ivz9OHxeKxnXi1nW0";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
