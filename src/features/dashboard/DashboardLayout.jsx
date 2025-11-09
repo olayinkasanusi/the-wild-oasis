6404e37e997806c7485f9a661a54ea040ac9c355
Sanusi Olayinka <222006189+olayinkasanusi@users.noreply.github.com>
2025-11-09 07:54:41 +0100

    //

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,3 +6,14 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+function DashboardLayout() {
+  return (
+    <StyledDashboardLayout>
+      <div>Stat</div>
+      <div>Activiy</div>
+      <div>Chart</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -43,7 +43,7 @@ const GlobalStyles = createGlobalStyle`
   }
   
   &.dark-mode {
-    --color-grey-0: #18212f;
+   --color-grey-0: #18212f;
 --color-grey-50: #111827;
 --color-grey-100: #1f2937;
 --color-grey-200: #374151;
